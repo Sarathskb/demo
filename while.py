d01db7200983621eb33b8dd2ac7e7899e1d8c05c
Sarath Kumar B <21sarathkumar2000@gmail.com>
2022-09-16 11:30:39 +0530

    boomer uncle

---
+++ while.py
@@ -0,0 +1,6 @@
+a= input("What you want?\n")
+b= input("Which help?\n")
+c = str(input("Enter the target name : "))
+d = "Balakrishnan"
+if (c == d):
+   print("Only one person can control him ..his name  is Mr. Nishnath ")
